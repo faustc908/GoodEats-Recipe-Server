886027afdde08a51c28c9f866d132e3375297212
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-05 14:45:32 -0400

    final update

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-/*if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/build")));
-}
-*/
 // CRUD routes
 
 // Route to test routing
